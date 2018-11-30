02ca1de5c4ee4fcd2b06ec900d3d5d1ef3a4a647
Christophe Nasarre <c.nasarresoulier@criteo.com>
2018-11-30 15:15:23 +0100

    Dump callstack when contention

--- criteo-build-centos7.sh
+++ criteo-build-centos7.sh
@@ -12,4 +12,4 @@ cd -
 
 ROOT=$(pwd)
 DOCKER_ARGS="run --env SOURCE_BUILD_SKIP_SUBMODULE_CHECK=1 -i --rm -v ${ROOT}:/mnt/source-build -w /mnt/source-build  microsoft/dotnet-buildtools-prereqs:centos-7-b46d863-20180719033416"
-docker ${DOCKER_ARGS}  /mnt/source-build/build.sh /p:RootRepo=core-setup
+docker ${DOCKER_ARGS}  /mnt/source-build/build.sh /p:RootRepo=core-setup /p:Configuration=Debug
