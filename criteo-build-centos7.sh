1de59544a516be4a3316bb23d80b7514fe37d3ca
Gregory LEOCADIE <g.leocadie@criteo.com>
2019-03-07 16:47:02 +0100

    Fix submodules update

--- criteo-build-centos7.sh
+++ criteo-build-centos7.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -xe
 
-git submodule update --init src/coreclr
-git submodule update --init src/corefx
-git submodule update --init src/standard
-git submodule update --init src/core-setup
-git submodule update --init src/newtonsoft-json
+git submodule update --init --recursive
 
 ROOT=$(pwd)
 DOCKER_ARGS="run --env SOURCE_BUILD_SKIP_SUBMODULE_CHECK=1 -i --rm -v ${ROOT}:/mnt/source-build -w /mnt/source-build  microsoft/dotnet-buildtools-prereqs:centos-7-b46d863-20180719033416"
