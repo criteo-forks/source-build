669011e2b193077f1701fc646d96595140a60aba
Gregory LEOCADIE <g.leocadie@criteo.com>
2019-03-11 11:31:27 +0100

    Update core-setup module and script
    define the package version in criteo-build-centos7.sh script

--- criteo-build-centos7.sh
+++ criteo-build-centos7.sh
@@ -4,4 +4,4 @@ git submodule update --init --recursive
 
 ROOT=$(pwd)
 DOCKER_ARGS="run --env SOURCE_BUILD_SKIP_SUBMODULE_CHECK=1 -i --rm -v ${ROOT}:/mnt/source-build -w /mnt/source-build  microsoft/dotnet-buildtools-prereqs:centos-7-b46d863-20180719033416"
-docker ${DOCKER_ARGS}  /mnt/source-build/build.sh /p:RootRepo=core-setup
+docker ${DOCKER_ARGS}  /mnt/source-build/build.sh /p:RootRepo=core-setup /p:VersionStamp='criteo1'
